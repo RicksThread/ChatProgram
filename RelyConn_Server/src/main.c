2c38317d60cb365f47df277576be9323a2561fcb
RicksThread <rick.sorelli@gmail.com>
2023-02-08 00:49:11 +0100

    server / client fixed thread

--- RelyConn_Server/src/main.c
+++ RelyConn_Server/src/main.c
@@ -1,20 +1,26 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <sys/time.h> 
+#include <string.h>
+#include <errno.h>
+#include "../../external/uthash/include/uthash.h"
 
-#define PORT 6969
+#define PORT 10000
 #define MAX_HOSTS 10
 
 #define BUFFER_LENGTH 1024
 
 #define FAILED_OP -1
 #define SUCCESS_OP 0
+
+#ifndef NULL
+#define NULL 0
+#endif
 
 struct sockaddr_in;
 typedef struct
@@ -25,37 +31,101 @@ typedef struct
 
 typedef struct
 {
-    bool active;
     int sock;
     struct sockaddr_in address;
     char buffer[BUFFER_LENGTH];
+    UT_hash_handle hh;
 }client_handle;
 
-pthread_t tid_read;
-pthread_t tid_write;
-pthread_t tid_listen;
 
 socket_local server_listener;
-client_handle clients[MAX_HOSTS];
+client_handle* clients[MAX_HOSTS];
+int clients_connected = 0;
 
-fd_set listener_set;
-int max_fd;
-
-void disconnect_client()
+bool is_client_active(client_handle* client)
 {
+    if (client == NULL)
+        return false;
 
+    return true;
 }
 
-void read_clients(void* args)
+void disconnect_client(int index)
 {
+    printf("Someone disconnected index: %d\n", index);
+    if (is_client_active(clients[index]) )
+    {
+        close(clients[index]->sock);
+        free(clients[index]);
+        clients_connected--;
+    }
+}
+
+void disconnect_clients()
+{
+    for(int i = 0; i < MAX_HOSTS; i++)
+    {
+        if(clients[i])
+        {
+            disconnect_client(i);
+        }
+    }
+}
+
+void* read_clients(void* args)
+{
+    int buffer_size = BUFFER_LENGTH;
+    fd_set listener_set;
+    int max_fd;
 
     while(1)
     {
+        FD_ZERO(&listener_set);
+
+
+        //fill the listener set to then check an activity of read between file descriptors
         for(int i = 0; i < MAX_HOSTS; i++)
         {
-            if (clients[i].active)
+            if (is_client_active(clients[i]))
             {
+                FD_SET(clients[i]->sock, &listener_set);
                 
+                if (clients[i]->sock > max_fd)
+                    max_fd = clients[i]->sock;
+                
+            }
+
+        }
+
+        int activity = select(max_fd +1, &listener_set, NULL, NULL, NULL);
+        if (activity <0 )
+        {
+            perror("select");
+            exit(EXIT_FAILURE);
+        }
+
+        //loop through all the players
+        for(int i = 0; i < MAX_HOSTS; i++)
+        {
+            if (is_client_active(clients[i]))
+            {
+                if (FD_ISSET(clients[i]->sock, &listener_set))
+                {
+                    //retrieves the message &
+                    //checks if the data format is correct
+
+                    int valread = read(clients[i]->sock, clients[i]->buffer, buffer_size);
+                    if (valread < 0)
+                    {
+                        //client has disconnected
+                        disconnect_client(i);
+                    }
+
+                    //as a test it echoes it back
+                    send(clients[i]->sock, clients[i]->buffer, strlen(clients[i]->buffer), 0);
+
+                    memset(clients[i]->buffer, 0, buffer_size);
+                }
             }
         }
     }
@@ -63,173 +133,153 @@ void read_clients(void* args)
 
 void write_clients(void* args)
 {
+    fd_set listener_set;
     int max_fd;
-
-    while(1)
+    while(true)
     {
+        FD_ZERO(&listener_set);
+        FD_SET(server_listener.sock, &listener_set);
+        max_fd = server_listener.sock;
+        
+
         for(int i = 0; i < MAX_HOSTS; i++)
         {
-            if (clients[i].active)
+            if (is_client_active(clients[i]))
             {
-
+                int sock = clients[i]->sock;
+                FD_SET(sock, &listener_set);
+                if (sock > max_fd)
+                    max_fd = sock;
             }
         }
+        
+        
+        int activity = select(max_fd + 1, &listener_set, NULL, NULL, NULL);
+        if ((activity < 0))
+            perror("select error");
     }
 }
 
-int init_server()
+void init_server()
 {
-    struct sockaddr_in* address = &server_listener.address;
-    int *sock = &server_listener.sock;
+    struct sockaddr_in address;
+    int sock;
     int opt = 1;
-    int addrlen = sizeof(*address);
 
-    *sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (*sock < 0)
+    if (sock < 0)
     {
-        perror("Socket failed");
-        return FAILED_OP;
-    }exit(EXIT_FAILURE);
-
+        perror("\nSocket failed\n");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0)
+    {
+        perror("set socket opt: ");
+        exit(EXIT_FAILURE);
+    }
     //uses the ipv4 protocol
-    address->sin_family = AF_INET;
+    address.sin_family = AF_INET;
 
     //enables to listen to any IP
-    address->sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = INADDR_ANY;
     
     //converts from host byte order to network byte order (idk what it is)
-    address->sin_port = htons(PORT);
+    address.sin_port = htons(PORT);
 
-    int success_bind = bind(*sock, (struct sockaddr*)&address, sizeof(address));
+    int success_bind = bind(sock, (struct sockaddr*)&address, (struct socklen_t*)sizeof(address));
     if (success_bind < 0)
     {
-        perror("bind has failed");
-        return FAILED_OP;
+        fprintf(stderr, "error code: %d; bind: %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
+    printf("Initialization of server");
+
+    server_listener.address = address;
+    server_listener.sock = sock;
     return SUCCESS_OP;
 }
 
-void add_user(client_handle handle)
+void add_user(client_handle* handle)
 {
     //add new socket to array of sockets 
     for (int i = 0; i < MAX_HOSTS; i++)  
     {  
         //if position is empty 
-        if(!clients[i].active)  
+        if(!is_client_active(clients[i]))  
         {  
-            clients[i] = handle;
-            clients[i].active = false;
             printf("Client added to list of sockets as %d\n" , i);  
+
+            clients[i] = handle;
+            clients_connected++;
             break;  
         }  
     }  
 }
 
-void listen_connections(void* args)
+void* listen_connections(void* args)
 {
-    client_handle newConnection;
+    printf("Start listening");
+
     int success_listen = listen(server_listener.sock, MAX_HOSTS);
-    int new_socket;
-
-
-    FD_ZERO(&listener_set);
-    FD_SET(server_listener.sock, &listener_set);  
 
     int size_addr = sizeof(server_listener.address);
     
     while(1)
     {
-        int activity = select(max_fd + 1, &listener_set, NULL, NULL, NULL);
-
-        if ((activity < 0))
+        client_handle* newConnection = malloc(sizeof(client_handle));
+        if ((newConnection->sock = accept(server_listener.sock, 
+                (struct sockaddr *)&newConnection->address, (socklen_t*)&size_addr))<0)  
         {  
-            printf("select error");  
+            perror("accept");
+            exit(EXIT_FAILURE);
         }
-
-        if (FD_ISSET(server_listener.sock, &listener_set))
+        else
         {
-            if ((newConnection.sock = accept(server_listener.sock, 
-                    (struct sockaddr *)&newConnection.address, (socklen_t*)&size_addr))<0)  
-            {  
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-
-            newConnection.active = true;
             add_user(newConnection);
 
-            if (newConnection.sock > max_fd)
-            {
-                max_fd = newConnection.sock;
-            }
-            
-            FD_SET(newConnection.sock, &listener_set);
         }
     }
-
-    
-    //inform user of socket number - used in send and receive commands 
-    printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , new_socket , inet_ntoa(newConnection.address.sin_addr) , ntohs(newConnection.address.sin_port));  
-
-    char* message = "sei stato scoccato\n";    
-    //send new connection greeting message 
-    if( send(new_socket, message, strlen(message), 0) != strlen(message) )  
-    {  
-        perror("send");  
-    }  
-        
-         
 }
 
 int main(int argc, char const* argv[])
 {
-    int success = init_server();
-    
-    if (success < 0)
+    pthread_t tid_listener, tid_reader;
+
+
+    init_server();
+    printf("\nserver initialized\n");
+    int success_listener = pthread_create(&tid_listener, NULL, &listen_connections, NULL);
+    if (success_listener <0 )
     {
-        perror("faluire initialization server");
+        fprintf(stderr, "failed: %s", strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
-    pthread_create(tid_listen, NULL, &listen_connections, NULL);
+    int success_reader = pthread_create(&tid_reader, NULL, &read_clients, NULL);
+    if (success_reader <0 )
+    {
+        fprintf(stderr, "failed: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    pthread_join(tid_listener, NULL);
+    pthread_join(tid_reader, NULL);
 
+    disconnect_clients();
+    
     /*
     valread = read(new_socket, buffer, sizeof(buffer));
     printf("%s\n", buffer);
-
-
     send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
-
-    sleep(5);
-
     close(new_socket);
-
+    
     */
 
     
-    shutdown(server_listener, SHUT_RDWR);
+    shutdown(server_listener.sock, SHUT_RDWR);
     
     return 0;
 }
-
-
-/*
-
-
-
-
-
-asincrono e sincrono
-
-sincorno e' quando gli eventi sono accordati su una base temporale
-
-asincrono è quando c'è una mancanza di sincronizzazione
-
-fourier
-shannon
-conversione analogico digitale
-quantizzazione
-
-*/
